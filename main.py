b6b700287e7f04f003ec6933c6121b6509544b29
alitalipcoskun <ali_talip_18@hotmail.com>
2025-02-03 18:01:01 +0300

    Test: logger class tested

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from src.logger import CustomLogger
+
 if __name__=="__main__":
-    print("Hello world!")
+    dict_test = {"test": "Bu bir deneme logudur"}
+    logger = CustomLogger()
+    logger.log(dict_test, "info")
